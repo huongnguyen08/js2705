24297cf880c2822f37a3463366f0515ff93c7552
Huong <huongnguyenak96@gmail.com>
2019-05-27 21:35:29 +0700

    b.js

---
+++ b.js
@@ -0,0 +1,28 @@
+// let arr = [ 1, 2, 3, 5 ];
+// console.log(typeof arr);
+
+// arr['index'] = 10;
+// console.log(arr.index);
+// console.log(arr);
+
+// console.log(arr);
+// push, pop, unshift, shift, splice
+// find, findIndex, map, forEach
+
+// let arr = new Array(1, 3, 5, 4, 22);
+
+let arr = [
+    { id: 1, name: 'Nam'},
+    { id: 2, name: 'Teo'}
+]
+//arrow fn
+let element = arr.find(value => value.id === 2)
+console.log(element);
+
+
+// let removed = arr.splice(1,1,[66,77]); 
+
+// arr.push(1); // add last
+// arr.unshift(1); // add first
+// arr.shift();
+
