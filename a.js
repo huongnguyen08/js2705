fda96a32a000bd8a539994755c218d7c8b13e503
Huong <huongnguyenak96@gmail.com>
2019-05-27 20:38:53 +0700

    a.js

--- a.js
+++ a.js
@@ -1,3 +1,38 @@
-let a = 1;
-console.log(a);
-console.log(a);
+// let a = 1;
+// a = 2;
+// var b = 'string';
+// console.log(b);
+
+// const PI = 3.14;
+// PI = 3.15; // error!
+
+// Khai bao hang so PI
+// const PI = 3.14;
+
+/**
+ * Khai bao hang so a
+ */
+// const a = 1;
+
+/**
+ * number 
+ * string
+ * boolean
+ * array
+ * obj 
+ * null
+ * undefined
+ */
+
+/** 
+ * return false 
+ * 0
+ * NaN
+ * ''
+ * false
+ * null
+ * undefined
+ */
+if([]) console.log('True')
+else console.log('False')
+
