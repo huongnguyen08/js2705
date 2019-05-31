b0f3feef87d41bb68660d2fc4d6fa3b57b8e02ac
Huong <huongnguyenak96@gmail.com>
2019-05-31 21:02:31 +0700

    vehinh2

--- function03.js
+++ function03.js
@@ -4,13 +4,16 @@
  *      * *         h=2,r=1,2
  *      * * *       h=3,r=1,2,3
  *      * * * *     h=4,r=1,2,3,4
- *      * * * * *
+ *      * * * * *   ==> r<=h
  * 
- *      * * * * *
- *      * * * *
- *      * * *
- *      * *
- *      *
+ *      * * * * *   h=1,r=12345 
+ *      * * * *     h=2,r=1234
+ *      * * *       h=3,r=123
+ *      * *         h=4,r=12
+ *      *           h=5,r=1
+ *                  ==> r+h <= 6 = 5 + 1 = h+1
+ * 
+ *      
  * 
  *      *
  *      * *
@@ -19,16 +22,26 @@
  *      * * * * *
  */
 
-function veHinh1(){
-    for(let h = 1; h<=5; h++){
+function veHinh1(h=5){
+    for(let y = 1; y<=h; y++){
         let str = '';
-        for(let r=1; r<=5;r++){
-            if(r<=h) str += '* '
-            // else console.log(' ')
+        for(let r=1; r<=h;r++){
+            if(r<=y) str += '* '
+            else str += ' '
         }
         console.log(str)
     }
 }
-veHinh1()
+function veHinh2(h=5){
+    for(let y = 1; y<=h; y++){
+        let str = '';
+        for(let r=1; r<=h;r++){
+            if(r+y <= h+1) str += '* '
+            else str += ' '
+        }
+        console.log(str)
+    }
+}
+veHinh2(6)
 
 
