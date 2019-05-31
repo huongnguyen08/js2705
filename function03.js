f6f8aa38fa7be6faaa3acf79221588944e2bcb1a
Huong <huongnguyenak96@gmail.com>
2019-05-31 20:37:51 +0700

    veHinh1

--- function03.js
+++ function03.js
@@ -1,9 +1,9 @@
 /**
  * Cho h = 5, r = 5
- *      *
- *      * *
- *      * * *
- *      * * * *
+ *      *           h=1,r=1
+ *      * *         h=2,r=1,2
+ *      * * *       h=3,r=1,2,3
+ *      * * * *     h=4,r=1,2,3,4
  *      * * * * *
  * 
  *      * * * * *
@@ -18,3 +18,17 @@
  *      *     *
  *      * * * * *
  */
+
+function veHinh1(){
+    for(let h = 1; h<=5; h++){
+        let str = '';
+        for(let r=1; r<=5;r++){
+            if(r<=h) str += '* '
+            // else console.log(' ')
+        }
+        console.log(str)
+    }
+}
+veHinh1()
+
+
